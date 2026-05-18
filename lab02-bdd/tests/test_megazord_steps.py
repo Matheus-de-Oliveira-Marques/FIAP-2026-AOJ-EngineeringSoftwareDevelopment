a3b263f1d10691253c962b32d0f187d20d77bcb8
Rafael Matsuyama <rafael.matsuyama@gmail.com>
2026-05-18 16:18:21 -0300

    fix: Lab 02

--- lab02-bdd/tests/test_megazord_steps.py
+++ lab02-bdd/tests/test_megazord_steps.py
@@ -8,20 +8,20 @@ scenarios('../features/megazord.feature')
 @given(parsers.parse('que o cliente é tipo "{tipo_cliente}"'), target_fixture="contexto")
 def setup_cliente(tipo_cliente):
     # Passamos os dados via um dicionário que funciona como a "memória" do teste
-    pass
+    raise NotImplementedError("Passo não implementado! Siga o roteiro (Passo 3).")
 
 @given(parsers.parse('a missão será na região "{regiao}"'))
 def setup_regiao(contexto, regiao):
-    pass
+    raise NotImplementedError("Passo não implementado! Siga o roteiro (Passo 3).")
 
 # WHEN: A Ação do Sistema
 @when(parsers.parse('o Megazord calcular o custo de uma missão de valor {valor_base:d}'))
 def acao_calcular(contexto, valor_base):
     # DICA: Instancie a CalculadoraDeEnergia aqui, chame o calcular_total
     # e salve o resultado dentro do "contexto", para que o THEN consiga validar.
-    pass
+    raise NotImplementedError("Passo não implementado! Siga o roteiro (Passo 3).")
 
 # THEN: A Validação (Asserts)
 @then(parsers.parse('o custo final de energia deve ser {valor_esperado:d}'))
 def checar_resultado(contexto, valor_esperado):
-    pass
+    raise NotImplementedError("Passo não implementado! Siga o roteiro (Passo 3).")
